1a0d46e7eb405466760b187665c7df2c5335bcb8
Mark Crowe <66536097+markcrowe-com@users.noreply.github.com>
2020-10-10 03:07:08 +0100

    Trees

---
+++ DataStructures.Implementation/src/main/java/com/markcrowe/datastructures/AvlTreeNodeComponent.java
@@ -0,0 +1,125 @@
+/*
+ * Copyright (c) 2020 Mark Crowe <https://github.com/markcrowe-com>. All rights reserved.
+ */
+package com.markcrowe.datastructures;
+
+import java.util.Comparator;
+
+/**
+ *
+ * @param <T>
+ * @param <TComparator>
+ */
+public class AvlTreeNodeComponent<T, TComparator extends Comparator<T>> implements AvlTreeNode<T>
+{
+	public AvlTreeNodeComponent(T value)
+	{
+		this.comparator = null;
+		this.leftTree = this.rightTree = null;  // this node has no children
+		this.value = value;
+	}
+	public AvlTreeNodeComponent(T value, TComparator comparator)
+	{
+		this.comparator = comparator;
+		this.leftTree = this.rightTree = null;  // this node has no children
+		this.value = value;
+	}
+	//
+	//	accessors
+	//
+	@Override
+	public AvlTreeNode<T> getLeftTree()
+	{
+		return this.leftTree;
+	}
+	@Override
+	public AvlTreeNode<T> getRightTree()
+	{
+		return this.rightTree;
+	}
+	@Override
+	public T getValue()
+	{
+		return this.value;
+	}
+
+	@Override
+	public AvlTreeNode<T> setLeftTree(AvlTreeNode<T> value)
+	{
+		this.leftTree = value;
+		return this.leftTree;
+	}
+	@Override
+	public AvlTreeNode<T> setRightTree(AvlTreeNode<T> value)
+	{
+		this.rightTree = value;
+		return this.rightTree;
+	}
+	/**
+	 *
+	 * @param value
+	 * @return
+	 */
+	@Override
+	public BinaryTreeNode<T> setLeftTree(BinaryTreeNode<T> value)
+	{
+		return setLeftTree((AvlTreeNode<T>) value);
+	}
+	@Override
+	public BinaryTreeNode<T> setRightTree(BinaryTreeNode<T> value)
+	{
+		return setRightTree((AvlTreeNode<T>) value);
+	}
+	//
+	//	modifiers
+	//
+	@Override
+	public synchronized AvlTreeNode<T> attachLeftValue(T value)
+	{
+		if(this.rightTree == null)
+		{
+			this.rightTree = this.CreateChildNode(value);
+			return this.rightTree;
+		}
+		else
+		{
+			return this.rightTree.attachLeftValue(value);
+		}
+	}
+	@Override
+	public synchronized AvlTreeNode<T> attachRightValue(T value)
+	{
+		if(this.leftTree == null)
+		{
+			return this.leftTree = this.CreateChildNode(value);
+		}
+		else
+		{
+			return this.leftTree.attachRightValue(value);
+		}
+	}
+	private AvlTreeNodeComponent<T, TComparator> CreateChildNode(T value)
+	{
+		return new AvlTreeNodeComponent<>(value, this.comparator);
+	}
+	//
+	//	Status Methods
+	//
+	@Override
+	public boolean isLeaf()
+	{
+		return (this.leftTree == null && this.rightTree == null);
+	}
+	@Override
+	public String toString()
+	{
+		return this.value.toString();
+	}
+	//
+	//	Private Fields
+	//
+	private final TComparator comparator;
+	private AvlTreeNode<T> leftTree;
+	private AvlTreeNode<T> rightTree;
+	private final T value;
+}
---
+++ DataStructures.Implementation/src/main/java/com/markcrowe/datastructures/BinaryTreeNodeComponent.java
@@ -0,0 +1,110 @@
+/*
+ * Copyright (c) 2020 Mark Crowe <https://github.com/markcrowe-com>. All rights reserved.
+ */
+package com.markcrowe.datastructures;
+
+import java.util.Comparator;
+
+/**
+ *
+ * @param <T>
+ * @param <TComparator>
+ */
+public class BinaryTreeNodeComponent<T, TComparator extends Comparator<T>> implements BinaryTreeNode<T>
+{
+	public BinaryTreeNodeComponent(T value)
+	{
+		this.comparator = null;
+		this.leftTree = this.rightTree = null;  // this node has no children
+		this.value = value;
+	}
+	public BinaryTreeNodeComponent(T value, TComparator comparator)
+	{
+		this.comparator = comparator;
+		this.leftTree = this.rightTree = null;  // this node has no children
+		this.value = value;
+	}
+	//
+	//	accessors
+	//
+	@Override
+	public BinaryTreeNode<T> getLeftTree()
+	{
+		return this.leftTree;
+	}
+	@Override
+	public BinaryTreeNode<T> getRightTree()
+	{
+		return this.rightTree;
+	}
+	@Override
+	public T getValue()
+	{
+		return this.value;
+	}
+
+	@Override
+	public BinaryTreeNode<T> setLeftTree(BinaryTreeNode<T> value)
+	{
+		this.leftTree = value;
+		return this.leftTree;
+	}
+	@Override
+	public BinaryTreeNode<T> setRightTree(BinaryTreeNode<T> value)
+	{
+		this.rightTree = value;
+		return this.rightTree;
+	}
+	//
+	//	modifiers
+	//
+	@Override
+	public synchronized BinaryTreeNode<T> attachLeftValue(T value)
+	{
+		if(this.rightTree == null)
+		{
+			this.rightTree = this.CreateChildNode(value);
+			return this.rightTree;
+		}
+		else
+		{
+			return this.rightTree.attachLeftValue(value);
+		}
+	}
+	@Override
+	public synchronized BinaryTreeNode<T> attachRightValue(T value)
+	{
+		if(this.leftTree == null)
+		{
+			return this.leftTree = this.CreateChildNode(value);
+		}
+		else
+		{
+			return this.leftTree.attachRightValue(value);
+		}
+	}
+	private BinaryTreeNodeComponent<T, TComparator> CreateChildNode(T value)
+	{
+		return new BinaryTreeNodeComponent<>(value, this.comparator);
+	}
+	//
+	//	Status Methods
+	//
+	@Override
+	public boolean isLeaf()
+	{
+		return (this.leftTree == null && this.rightTree == null);
+	}
+	@Override
+	public String toString()
+	{
+		return this.value.toString();
+	}
+	//
+	//	Private Fields
+	//
+	private final TComparator comparator;
+	private BinaryTreeNode<T> leftTree;
+	private BinaryTreeNode<T> rightTree;
+	private final T value;
+}
